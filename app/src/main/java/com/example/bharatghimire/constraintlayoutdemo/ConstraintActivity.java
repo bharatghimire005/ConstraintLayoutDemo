1d3f8a6cd5a8c65113f11197118d749c236bdae9
Bharat Ghimire <bharat.ghimire@tothenew.com>
2017-10-28 12:52:49 +0530

    Added the view for the constraint layout

--- app/src/main/java/com/example/bharatghimire/constraintlayoutdemo/ConstraintActivity.java
+++ app/src/main/java/com/example/bharatghimire/constraintlayoutdemo/ConstraintActivity.java
@@ -8,6 +8,6 @@ public class ConstraintActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_constraint);
+        setContentView(R.layout.layout_demo_view_old);
     }
 }
